e7716b3f7400c74b46b1b6fba5e3d54a35fe688d
raannakasturi <raannakasturi@gmail.com>
2024-11-24 21:26:54 +0530

    refactor: update useEffect dependency to include globeRef.current in globe component

--- src/components/ui/globe.tsx
+++ src/components/ui/globe.tsx
@@ -97,7 +97,7 @@ export function Globe({ globeConfig, data }: WorldProps) {
       _buildData();
       _buildMaterial();
     }
-  });
+  }, [globeRef.current]);
 
   const _buildMaterial = () => {
     if (!globeRef.current) return;
