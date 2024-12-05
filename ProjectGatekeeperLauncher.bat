3b4a9998bbb221541faa2020b4de927b9fa8b488
Nayan Kasturi <raannakasturi@gmail.com>
2024-12-05 16:33:24 +0530

    Add files via upload

---
+++ ProjectGatekeeperLauncher.bat
@@ -0,0 +1,14 @@
+@echo off
+
+:: Path to Chrome executable
+set CHROME_PATH="C:\Program Files\Google\Chrome\Application\chrome.exe"
+
+:: Open each URL in a new tab
+start "" %CHROME_PATH% "https://react-icons.github.io/react-icons/"
+start "" %CHROME_PATH% "https://tailwindcss.com/docs/installation"
+start "" %CHROME_PATH% "https://ui.aceternity.com/components"
+start "" %CHROME_PATH% "https://webflow.com/made-in-webflow/cybersecurity"
+start "" %CHROME_PATH% "https://dribbble.com/tags/cyber-security-website"
+start "" %CHROME_PATH% "https://www.behance.net/search/projects/cyber security website"
+start "" %CHROME_PATH% "https://projectgatekeeper.vercel.app/"
+start "" %CHROME_PATH% "http://localhost:3000/"
